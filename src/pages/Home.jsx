89a711ca839efc8d1eed035169301203531db1f1
Julia <kret.julia@gmail.com>
2025-05-12 11:59:43 +0200

    liked button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [liked, setLiked] = useState(false);
 
   const token = localStorage.getItem("token");
 
@@ -16,7 +17,6 @@ function Home() {
       await axios.delete(`http://localhost:3001/api/events/${id}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-      // console.log("Hallo", headers);
       setEvents(events.filter((event) => event.id !== id)); // Lokalen Zustand aktualisieren
     } catch (error) {
       console.error("Fehler beim Löschen:", error);
@@ -42,6 +42,12 @@ function Home() {
 
   console.log(events);
 
+  // function LikeButton() {
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className="px-8 py-6 bg-gray-100 min-h-screen">
       <h2 className="text-3xl font-bold mb-6 text-gray-400 text-center">
@@ -62,16 +68,44 @@ function Home() {
                 {event.title}
               </h3>
               <p className="text-gray-300">Datum: {event.date}</p>
-              <p className="text-gray-300 ">
-                Beschreibung: {event.description}
-              </p>
+              {/* <p className="text-gray-300 "> */}
+              {/* Beschreibung: {event.description} */}
+              {/* </p> */}
               <p className="text-gray-300 ">Location: {event.location}</p>
-              <p className="text-gray-300 ">S || N: {event.latitude}</p>
-              <p className="text-gray-300 ">O || W: {event.longitude}</p>
-              <Link to={`/events/${event.id}`}>DETAILS HIER LANG...</Link>
+              {/* <p className="text-gray-300 ">S || N: {event.latitude}</p> */}
+              {/* <p className="text-gray-300 ">O || W: {event.longitude}</p> */}
+              <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+                <Link to={`/events/${event.id}`}>DETAILS HIER LANG...</Link>
+              </button>
+
               <p>
-                <button onClick={() => deleteEntry(event.id)} className="">
+                <button
+                  onClick={() => deleteEntry(event.id)}
+                  className="btn btn-error cursor-pointer"
+                >
                   Eintrag löschen
+                </button>
+              </p>
+
+              <p>
+                <button
+                  className="btn btn-square cursor-pointer"
+                  onClick={toggleLike}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill={liked ? "red" : "none"}
+                    viewBox="0 0 24 24"
+                    strokeWidth="2.5"
+                    stroke="currentColor"
+                    className="size-[1.2em]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
                 </button>
               </p>
             </div>
